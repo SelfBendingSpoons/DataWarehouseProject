5c89baf779565e35ab2eae109db46ed5479a4f11
SelfBendingSpoons <blake@thestandifers.com>
2026-08-28 22:16:28 -0500

    Update create_bronze_tables.sql

--- scripts/bronze/create_bronze_tables.sql
+++ scripts/bronze/create_bronze_tables.sql
@@ -20,7 +20,7 @@ CREATE TABLE bronze.crm_cust_info (
 
 IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
-CREATE TABLE bronze.crm_prd_info; (
+CREATE TABLE bronze.crm_prd_info (
 	prd_id			INT,
 	prd_key 		NVARCHAR(50),
 	prd_nm			NVARCHAR(50),
